593c53ecaefe119d6711f9cba2608d58501c6e12
thomenat <156375450+thomenat@users.noreply.github.com>
2024-07-24 19:08:33 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ import './App.css';
 export default function App() {
   return (
     <div className='App'>
-      Weather App
+      <div className='container'>
+     <h1>Weather App</h1>
+
+    <footer>
+      This project was coded by <a href='https://www.linkedin.com/in/natalia-thome/' target='_blank'>Natália Thomé</a> and is {""}
+      <a href='https://github.com/thomenat/my-clima-react' target='_blank'>
+      open-sourced on GitHub
+      </a>
+    </footer>
+
+    </div>
     </div>
   );
 }
