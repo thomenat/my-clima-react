fcc7c0559ec11375e37a64ddc40dcc85cc38dcb2
thomenat <156375450+thomenat@users.noreply.github.com>
2024-07-27 18:29:32 +0100

    added basic css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
+import Weather from './Weather';
 import './App.css';
 
 export default function App() {
   return (
     <div className='App'>
-      <div className='container'>
-     <h1>Weather App</h1>
-
-    <footer>
-      This project was coded by <a href='https://www.linkedin.com/in/natalia-thome/' target='_blank'>Natália Thomé</a> and is {""}
-      <a href='https://github.com/thomenat/my-clima-react' target='_blank'>
+    <div className='container'>
+    <Weather />
+    <footer className='footer'>
+      This project was coded by <a href='https://www.linkedin.com/in/natalia-thome/' target='_blank' rel='noreferrer'>Natália Thomé</a> and is {""}
+      <a href='https://github.com/thomenat/my-clima-react' target='_blank' rel='noreferrer'>
       open-sourced on GitHub
       </a>
     </footer>
